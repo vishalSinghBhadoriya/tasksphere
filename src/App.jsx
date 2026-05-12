ea0b34632bfd5d1ebc7746976cd28847ed14c8f0
visha bhadoriya <vishalbhadoriya9765@gmail.com>
2026-05-12 14:14:44 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center text-4xl font-bold">
+      TaskSphere
+    </div>
+  )
+}
+
+export default App
